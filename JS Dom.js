7c59d9de4b0cc02eceee5e3ac14be66df7fdeccf
duorfan <nf2111@nyu.edu>
2021-11-04 16:58:05 -0400

    v1

--- JS Dom.js
+++ JS Dom.js
@@ -1,3 +1,18 @@
-document.getElementById("first").innerHTML = "!";
-document.getElementById("second").innerHTML = "!";
-document.getElementById("third").innerHTML = "!";
+const firstElements = document.getElementsByTagName("p");
+const firstCount = firstElements.length;
+console.log("There are " +firstCount+" paragraph tags on this page.");
+function myFunction1(){
+    document.getElementById("first").innerHTML = firstCount;
+}
+
+
+const secondCount = document.querySelectorAll("#pick p").length;
+function myfunction2(){
+    document.getElementById("second").innerHTML = secondCount;
+}
+
+const thirdElements = document.getElementsByID("choose");
+const thirdCount = thirdElements.length;
+function myfunction3(){
+document.getElementById("third").innerHTML = thirdCount;
+}
